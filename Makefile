91ceef6c5f3ad1bda1bd697898d59e098c0ad9a7
Omkar <djomi400@gmail.com>
2015-10-14 06:50:12 -0400

    build script added && modified it with my own way

--- Makefile
+++ Makefile
@@ -198,7 +198,7 @@ SUBARCH := $(shell uname -m | sed -e s/i.86/i386/ -e s/sun4u/sparc64/ \
 # Note: Some architectures assign CROSS_COMPILE in their arch/*/Makefile
 export KBUILD_BUILDHOST := $(SUBARCH)
 ARCH		?= arm
-CROSS_COMPILE	?= /home/gustavo/toolchain/bin/arm-linux-androideabi-
+CROSS_COMPILE	?= /home/omkar/kernel/uber/bin/arm-eabi-
 
 # Architecture as present in compile.h
 UTS_MACHINE 	:= $(ARCH)
@@ -1576,3 +1576,4 @@ FORCE:
 # Declare the contents of the .PHONY variable as phony.  We keep that
 # information in a variable so we can use it in if_changed and friends.
 .PHONY: $(PHONY)
+
--- build.sh
+++ build.sh
@@ -1,37 +1,54 @@
 #!/bin/bash
 
-KERNEL_DIR="/home/gustavo/kernel-msm"
-STRIP="/home/gustavo/toolchain/bin/arm-linux-androideabi-strip"
-MODULES=("/home/gustavo/kernel-msm/drivers/char/adsprpc.ko" "/home/gustavo/kernel-msm/drivers/scsi/scsi_wait_scan.ko" "/home/gustavo/kernel-msm/drivers/misc/utag/utags.ko" "/home/gustavo/kernel-msm/drivers/staging/prima_mmi/wlan.ko" )
-MODULES_DIR="/home/gustavo/kernel-msm/out/anykernel2/modules"
-ZIMAGE="/home/gustavo/kernel-msm/arch/arm/boot/zImage"
-ZIMAGE2="/home/gustavo/kernel-msm/out/anykernel2/zImage"
-ZIP_DIR="/home/gustavo/kernel-msm/out/anykernel2"
-CURRENTDATE=$(date +"%d-%m")
+###### defines ######
 
+local_dir=$PWD
+defconfig=msm8960dt_mmi_defconfig
+jobs=4
 
-case "$1" in
-	clean)
-        cd ${KERNEL_DIR}
-        make clean && make mrproper
-		;;
-	build)
-        # build the kernel
-        cd ${KERNEL_DIR}
-        cp ./arch/arm/configs/msm8960dt_mmi_defconfig ./.config
-        make -j3
-        # strip modules
-        for module in "${MODULES[@]}" ; do
-            cp "${module}" ${MODULES_DIR}
-            ${STRIP} --strip-unneeded ${MODULES_DIR}/*
-        done
-        # build boot.img
-        cp ${ZIMAGE} ${ZIMAGE2}
-		echo "Creating kernel zip..."
-		cd ${ZIP_DIR}
-        zip -r Kernel-$CURRENTDATE.zip ./ -x *.zip *.gitignore *EMPTY_DIRECTORY
-        rm ${ZIMAGE2}
-
-		echo "Done!"
-	    ;;
-esac
+###### defines ######
+echo '#############'
+echo 'making clean'
+echo '#############'
+make clean                                                                 # clean the sources
+rm -rf out                                                                 # clean the output folder
+echo '#############'
+echo 'making defconfig'
+echo '#############'
+make $defconfig
+echo '#############'
+echo 'making zImage'
+echo '#############'
+time make -j$jobs
+echo '#############'
+echo 'copying files to ./out'
+echo '#############'
+echo ''
+mkdir -p out/modules                                                       # make dirs for zImage and modules
+cp arch/arm/boot/zImage out/zImage                                         # copy zImage
+# Find and copy modules
+find -name '*.ko' | xargs -I {} cp {} ./out/modules/
+cp -r out/* ~/kernel/anykernel/                                              # copy zImage and modules to a my folder
+echo 'done'
+echo ''
+if [ -a arch/arm/boot/zImage ]; then
+echo '#############'
+echo 'Making Anykernel zip'
+echo '#############'
+echo ''
+cd ~/kernel/anykernel/ 
+zip -r Kernel-$CURRENTDATE.zip ./ -x *.zip *.gitignore *EMPTY_DIRECTORY
+if [[ $1 = -d ]]; then
+cp $zipname ~/kernel/anykernel/$zipname
+echo "Copying $zipname to My Folder"
+fi
+cd $local_dir                                                              # cd back to the old dir
+echo ''
+echo '#############'
+echo 'build finished successfully'
+echo '#############'
+else
+echo '#############'
+echo 'build failed!'
+echo '#############'
+fi
