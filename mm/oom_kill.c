0f416024424680fb4abac69af29ceeaec933f79a
Omkar <djomi400@gmail.com>
2015-10-29 07:30:23 -0400

    fix derp

--- mm/oom_kill.c
+++ mm/oom_kill.c
@@ -196,7 +196,6 @@ unsigned long oom_badness(struct task_struct *p, struct mem_cgroup *memcg,
 			  const nodemask_t *nodemask, unsigned long totalpages)
 {
 	unsigned long points;
-	long points;
 	long adj;
 
 	if (oom_unkillable_task(p, memcg, nodemask))
