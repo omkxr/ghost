2292d1779314e6810982bfdaeae65eb8cfbc7b8b
Omkar <djomi400@gmail.com>
2016-02-27 07:19:46 +0000

    import my build script

---
+++ build.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+echo Exporting Your User Name
+export KBUILD_BUILD_USER=omkar
+
+echo  Exporting Your Host Name
+export KBUILD_BUILD_HOST=megatron007
+
+###### defines ######
+
+local_dir=$PWD
+defconfig=moto_x_tycoo_defconfig
+jobs=4
+
+###### defines ######
+echo '#############'
+echo 'making clean'
+echo '#############'
+make clean && mrproper                                                                # clean 
+the sources
+rm -rf out                                                                 # clean the output 
+folder
+echo '#############'
+echo 'making defconfig'
+echo '#############'
+make $defconfig
+echo '#############'
+echo 'making zImage'
+echo '#############'
+time make -j$jobs
+echo '#############'
+echo 'copying files to ./out'
+echo '#############'
+echo ''
+mkdir -p out/modules                                                       # make dirs for 
+zImage and modules
+cp arch/arm/boot/zImage out/zImage                                         # copy zImage
+# Find and copy modules
+find -name '*.ko' | xargs -I {} cp {} ./out/modules/
+cp -r out/* ~/kernel/anykernel/                                              # copy zImage and 
+modules to a my folder
+echo 'done'
+echo ''
+if [ -a arch/arm/boot/zImage ]; then
+echo '#############'
+echo 'Making Anykernel zip'
+echo '#############'
+echo ''
+cd ~/kernel/anykernel/ 
+zip -r MeGaByTe-kernel-r4.zip ./ -x *.zip *.gitignore *EMPTY_DIRECTORY
+if [[ $1 = -d ]]; then
+cp $zipname ~/kernel/anykernel/$zipname
+echo "Copying $zipname to My Folder"
+fi
+cd $local_dir                                                              # cd back to the old 
+dir
+echo ''
+echo '#############'
+echo 'build finished successfully'
+echo '#############'
+else
+echo '#############'
+echo 'build failed!'
+echo '#############'
+fi
